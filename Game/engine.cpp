499257aadee205aa6886a3ca8b148a5914cc6f6f
mmelton514 <84087957+mmelton514@users.noreply.github.com>
2025-12-23 18:33:27 -0600

    Added some more cpp and headers

---
+++ Game/engine.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+
+#include "engine.h"
+
+using namespace std;
+
+int Engine::game_loop()
+{
+	update();
+	draw();
+	return 0;
+}
+
+void Engine::update()
+{
+
+}
+
+void Engine::draw()
+{
+
+}
+
+void Engine::start_game()
+{
+
+}
---
+++ Game/engine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Engine
+{
+private :
+
+	int game_loop();
+
+	void update();
+
+	void draw();
+
+	void start_game();
+};
